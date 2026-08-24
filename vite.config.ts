b8a79848a6a11750584d3e55c293eb430b07bf06
spixelsdev <yhogieisk66@gmail.com>
2026-08-24 19:43:39 +0200

    ci: add github pages auto-deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nusantara-region-explorer/',
   plugins: [
     vue(),
     tailwindcss()
